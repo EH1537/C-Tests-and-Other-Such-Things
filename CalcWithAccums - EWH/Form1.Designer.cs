eb4f8417de5397fef6de330bc39c0a765c5031c6
EH1537 <53024657+EH1537@users.noreply.github.com>
2019-07-22 18:51:25 -0400

    Add files via upload

---
+++ CalcWithAccums - EWH/Form1.Designer.cs
@@ -0,0 +1,431 @@
+﻿namespace CalcWithAccums___EWH
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Lin = new System.Windows.Forms.TextBox();
+            this.Win = new System.Windows.Forms.TextBox();
+            this.Hin = new System.Windows.Forms.TextBox();
+            this.CalcButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ClearAll = new System.Windows.Forms.Button();
+            this.ClearIn = new System.Windows.Forms.Button();
+            this.OrdersOut = new System.Windows.Forms.Label();
+            this.TVOut = new System.Windows.Forms.Label();
+            this.TPout = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.CuFeetAccumOut = new System.Windows.Forms.Label();
+            this.TVAccumOut = new System.Windows.Forms.Label();
+            this.TPAccumOut = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.CuOut = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.ETVol = new System.Windows.Forms.Label();
+            this.VOOut = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.VORemOut = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Lin
+            // 
+            this.Lin.Location = new System.Drawing.Point(104, 58);
+            this.Lin.Name = "Lin";
+            this.Lin.Size = new System.Drawing.Size(100, 20);
+            this.Lin.TabIndex = 0;
+            // 
+            // Win
+            // 
+            this.Win.Location = new System.Drawing.Point(104, 84);
+            this.Win.Name = "Win";
+            this.Win.Size = new System.Drawing.Size(100, 20);
+            this.Win.TabIndex = 1;
+            // 
+            // Hin
+            // 
+            this.Hin.Location = new System.Drawing.Point(104, 110);
+            this.Hin.Name = "Hin";
+            this.Hin.Size = new System.Drawing.Size(100, 20);
+            this.Hin.TabIndex = 2;
+            // 
+            // CalcButton
+            // 
+            this.CalcButton.Location = new System.Drawing.Point(242, 55);
+            this.CalcButton.Name = "CalcButton";
+            this.CalcButton.Size = new System.Drawing.Size(75, 23);
+            this.CalcButton.TabIndex = 3;
+            this.CalcButton.Text = "Calculate";
+            this.CalcButton.UseVisualStyleBackColor = true;
+            this.CalcButton.Click += new System.EventHandler(this.CalcButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Length (ft)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Width (ft)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Height (ft)";
+            // 
+            // ClearAll
+            // 
+            this.ClearAll.Location = new System.Drawing.Point(242, 113);
+            this.ClearAll.Name = "ClearAll";
+            this.ClearAll.Size = new System.Drawing.Size(75, 23);
+            this.ClearAll.TabIndex = 19;
+            this.ClearAll.Text = "Clear";
+            this.ClearAll.UseVisualStyleBackColor = true;
+            this.ClearAll.Click += new System.EventHandler(this.ClearAll_Click);
+            // 
+            // ClearIn
+            // 
+            this.ClearIn.Location = new System.Drawing.Point(242, 84);
+            this.ClearIn.Name = "ClearIn";
+            this.ClearIn.Size = new System.Drawing.Size(75, 23);
+            this.ClearIn.TabIndex = 20;
+            this.ClearIn.Text = "Clear Entries";
+            this.ClearIn.UseVisualStyleBackColor = true;
+            this.ClearIn.Click += new System.EventHandler(this.ClearIn_Click);
+            // 
+            // OrdersOut
+            // 
+            this.OrdersOut.AutoEllipsis = true;
+            this.OrdersOut.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.OrdersOut.Location = new System.Drawing.Point(104, 156);
+            this.OrdersOut.Name = "OrdersOut";
+            this.OrdersOut.Size = new System.Drawing.Size(100, 23);
+            this.OrdersOut.TabIndex = 21;
+            // 
+            // TVOut
+            // 
+            this.TVOut.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.TVOut.Location = new System.Drawing.Point(73, 70);
+            this.TVOut.Name = "TVOut";
+            this.TVOut.Size = new System.Drawing.Size(100, 23);
+            this.TVOut.TabIndex = 8;
+            // 
+            // TPout
+            // 
+            this.TPout.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.TPout.Location = new System.Drawing.Point(73, 123);
+            this.TPout.Name = "TPout";
+            this.TPout.Size = new System.Drawing.Size(100, 23);
+            this.TPout.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Cubic Feet";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 79);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Cubic Yards";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(17, 120);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 26);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Cost at \r\n$17/yard";
+            // 
+            // CuFeetAccumOut
+            // 
+            this.CuFeetAccumOut.AutoEllipsis = true;
+            this.CuFeetAccumOut.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.CuFeetAccumOut.Location = new System.Drawing.Point(278, 18);
+            this.CuFeetAccumOut.Name = "CuFeetAccumOut";
+            this.CuFeetAccumOut.Size = new System.Drawing.Size(100, 23);
+            this.CuFeetAccumOut.TabIndex = 13;
+            // 
+            // TVAccumOut
+            // 
+            this.TVAccumOut.AutoEllipsis = true;
+            this.TVAccumOut.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.TVAccumOut.Location = new System.Drawing.Point(278, 69);
+            this.TVAccumOut.Name = "TVAccumOut";
+            this.TVAccumOut.Size = new System.Drawing.Size(100, 23);
+            this.TVAccumOut.TabIndex = 14;
+            // 
+            // TPAccumOut
+            // 
+            this.TPAccumOut.AutoEllipsis = true;
+            this.TPAccumOut.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.TPAccumOut.Location = new System.Drawing.Point(278, 126);
+            this.TPAccumOut.Name = "TPAccumOut";
+            this.TPAccumOut.Size = new System.Drawing.Size(100, 23);
+            this.TPAccumOut.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(220, 124);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 26);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Cost for \r\nAll Orders";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(208, 70);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(67, 26);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Cubic Yards\r\nfor All Orders";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(214, 18);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(67, 26);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Cubic Feet\r\nfor All Orders";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.CuOut);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.TPAccumOut);
+            this.groupBox1.Controls.Add(this.TVAccumOut);
+            this.groupBox1.Controls.Add(this.CuFeetAccumOut);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.TPout);
+            this.groupBox1.Controls.Add(this.TVOut);
+            this.groupBox1.Location = new System.Drawing.Point(31, 214);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(424, 164);
+            this.groupBox1.TabIndex = 23;
+            this.groupBox1.TabStop = false;
+            // 
+            // CuOut
+            // 
+            this.CuOut.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.CuOut.Location = new System.Drawing.Point(73, 21);
+            this.CuOut.Name = "CuOut";
+            this.CuOut.Size = new System.Drawing.Size(100, 23);
+            this.CuOut.TabIndex = 32;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(24, 156);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(74, 26);
+            this.label10.TabIndex = 24;
+            this.label10.Text = "Total Number \r\nof Orders";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(115, 198);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(70, 13);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "Current Order";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(330, 198);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(52, 13);
+            this.label12.TabIndex = 26;
+            this.label12.Text = "All Orders";
+            // 
+            // label13
+            // 
+            this.label13.CausesValidation = false;
+            this.label13.Location = new System.Drawing.Point(4, 404);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(85, 46);
+            this.label13.TabIndex = 27;
+            this.label13.Text = "Empty Truck \r\nVolume \r\n(3 Cubic Yards)";
+            this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ETVol
+            // 
+            this.ETVol.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.ETVol.Location = new System.Drawing.Point(95, 414);
+            this.ETVol.Name = "ETVol";
+            this.ETVol.Size = new System.Drawing.Size(64, 23);
+            this.ETVol.TabIndex = 19;
+            // 
+            // VOOut
+            // 
+            this.VOOut.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.VOOut.Location = new System.Drawing.Point(233, 415);
+            this.VOOut.Name = "VOOut";
+            this.VOOut.Size = new System.Drawing.Size(64, 23);
+            this.VOOut.TabIndex = 28;
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(182, 414);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(45, 26);
+            this.label16.TabIndex = 29;
+            this.label16.Text = "Volume \r\nOrdered";
+            this.label16.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // VORemOut
+            // 
+            this.VORemOut.BackColor = System.Drawing.Color.LightGray;
+            this.VORemOut.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.VORemOut.Location = new System.Drawing.Point(370, 415);
+            this.VORemOut.Name = "VORemOut";
+            this.VORemOut.Size = new System.Drawing.Size(64, 23);
+            this.VORemOut.TabIndex = 30;
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(307, 414);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(57, 26);
+            this.label18.TabIndex = 31;
+            this.label18.Text = "Volume \r\nRemaining";
+            this.label18.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 493);
+            this.Controls.Add(this.VORemOut);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.VOOut);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.ETVol);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.OrdersOut);
+            this.Controls.Add(this.ClearIn);
+            this.Controls.Add(this.ClearAll);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CalcButton);
+            this.Controls.Add(this.Hin);
+            this.Controls.Add(this.Win);
+            this.Controls.Add(this.Lin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Lin;
+        private System.Windows.Forms.TextBox Win;
+        private System.Windows.Forms.TextBox Hin;
+        private System.Windows.Forms.Button CalcButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button ClearAll;
+        private System.Windows.Forms.Button ClearIn;
+        private System.Windows.Forms.Label OrdersOut;
+        private System.Windows.Forms.Label TVOut;
+        private System.Windows.Forms.Label TPout;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label CuFeetAccumOut;
+        private System.Windows.Forms.Label TVAccumOut;
+        private System.Windows.Forms.Label TPAccumOut;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label ETVol;
+        private System.Windows.Forms.Label VOOut;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label VORemOut;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label CuOut;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ CalcWithAccums - EWH/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+/*
+EWH 
+7.22.19
+    */
+namespace CalcWithAccums___EWH
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        const decimal Yardage = 27m; // a cubic yard is 27 cubic feet
+        const decimal Pricage = 17m; // a cubic yard is $17
+        const decimal TruckBed = 4m; // a truck load is 4 cubic yards
+
+        //accumulation holds
+        decimal TVaccum = 0;  //total volume (yards) accumulated
+        decimal CuFeetaccum = 0; //total cubic feet accumulated
+        decimal TPaccum = 0; //total price accumulated
+        decimal Ordersaccum = 0; //total orders accumulated
+        decimal VolumeRemaining = 0; //volume left in trucks
+
+
+
+
+        private void CalcButton_Click(object sender, EventArgs e)
+        {
+            //my input holds 
+            decimal LCalc = 0;
+            decimal HCalc = 0;
+            decimal WCalc = 0;
+
+
+            //my calculation holds
+            decimal CuFeetCalc = 0; //cubic feet
+            decimal TVCalc = 0; //total volume (yards)
+            decimal TPCalc = 0; //total price 
+
+            try
+            {
+                LCalc = decimal.Parse(Lin.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Please Enter a valid Length");
+                return;
+            }
+            try
+            {
+                WCalc = decimal.Parse(Win.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Please Enter a valid Width");
+                return;
+            }
+            try
+            {
+                HCalc = decimal.Parse(Hin.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Please Enter a valid Height");
+                return;
+            }
+
+       
+
+
+
+            //calcuations (none accumulated)
+            CuFeetCalc = LCalc * WCalc * HCalc;
+            TVCalc = CuFeetCalc / Yardage;
+            TPCalc = TVCalc * Pricage;
+
+            //calculations (accumulated)
+            CuFeetaccum = CuFeetaccum + CuFeetCalc;
+            TVaccum = TVaccum + TVCalc;
+            TPaccum = TVaccum * Pricage;
+            Ordersaccum = Ordersaccum + 1;
+            VolumeRemaining = TruckBed - TVaccum;
+
+
+            //calculations to outputs
+            CuOut.Text = CuFeetCalc.ToString("n2");
+            TVOut.Text = TVCalc.ToString("n2");
+            TPout.Text = TPCalc.ToString("n2");
+            CuFeetAccumOut.Text = CuFeetaccum.ToString("n2");
+            TPAccumOut.Text = TPaccum.ToString("c");
+            TVAccumOut.Text = TVaccum.ToString("n2");
+            OrdersOut.Text = Ordersaccum.ToString();
+            ETVol.Text = TruckBed.ToString();
+            VOOut.Text = TVaccum.ToString("n2");
+            VORemOut.Text = VolumeRemaining.ToString("n2");
+
+            if (TruckBed > TVaccum) //the IF ELSE statement depending on whci calculated cost is greater
+                VORemOut.BackColor = Color.PaleTurquoise; //if the Left is greater than the right, left is Red
+            else
+                VORemOut.BackColor = Color.Red;
+
+          
+        }
+
+        private void ClearAll_Click(object sender, EventArgs e)
+        {
+            Lin.Text = null;
+            Win.Text = null;
+            Hin.Text = null;
+            CuOut.Text = null;
+            TVOut.Text = null;
+            TPout.Text = null;
+            CuFeetAccumOut.Text = null;
+            TPAccumOut.Text = null;
+            TVAccumOut.Text = null;
+            OrdersOut.Text = null;
+            ETVol.Text = null;
+            VOOut.Text = null;
+            VORemOut.Text = null;
+            TVaccum = 0; 
+            CuFeetaccum = 0; 
+            TPaccum = 0; 
+            Ordersaccum = 0; 
+            VolumeRemaining = 0;
+            VORemOut.BackColor = Color.LightGray;
+
+
+        }
+
+        private void ClearIn_Click(object sender, EventArgs e)
+        {
+            Lin.Text = null;
+            Win.Text = null;
+            Hin.Text = null;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
+
+  
